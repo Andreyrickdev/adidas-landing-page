36e483d928377222d610223c6bbf10ebc044c47f
Andreyrickdev <contato-andrey@outlook.com>
2022-08-12 11:40:15 -0300

    added event listener on account and label.classList.add

--- assets/js/myInput.js
+++ assets/js/myInput.js
@@ -1,3 +1,8 @@
 const account = document.getElementsByClassName('account')[0];
 const input = account.getElementsByTagName('input')[0];
 const label = account.getElementsByTagName('label')[0];
+
+account.addEventListener('click', function () {
+
+    label.classList.add('active');
+});
