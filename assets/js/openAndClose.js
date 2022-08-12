aa6c029eaf695677ada295e568c9673d1dd88cc1
Andreyrickdev <contato-andrey@outlook.com>
2022-08-12 11:52:06 -0300

    selected class user-li

--- assets/js/openAndClose.js
+++ assets/js/openAndClose.js
@@ -1,2 +1,3 @@
 const hamburgButton = document.getElementsByClassName('hamburger-li')[0];
 const searchButton = document.getElementsByClassName('buttonSearch')[0];
+const searchUser = document.getElementsByClassName('user-li')[0];
