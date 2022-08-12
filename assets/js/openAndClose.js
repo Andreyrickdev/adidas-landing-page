f65e47a86d94ea36719ee6655498a485102e0819
Andreyrickdev <contato-andrey@outlook.com>
2022-08-12 11:52:25 -0300

    selected class heart-li

--- assets/js/openAndClose.js
+++ assets/js/openAndClose.js
@@ -1,3 +1,4 @@
 const hamburgButton = document.getElementsByClassName('hamburger-li')[0];
 const searchButton = document.getElementsByClassName('buttonSearch')[0];
 const searchUser = document.getElementsByClassName('user-li')[0];
+const favoriteButton = document.getElementsByClassName('heart-li')[0];
