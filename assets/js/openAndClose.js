4c87de4dd1b280a9cc51ec02f01483062d380b4c
Andreyrickdev <contato-andrey@outlook.com>
2022-08-12 11:57:50 -0300

    selected class nav-mobile and created function openOrClose

--- assets/js/openAndClose.js
+++ assets/js/openAndClose.js
@@ -4,3 +4,8 @@ const searchUser = document.getElementsByClassName('user-li')[0];
 const favoriteButton = document.getElementsByClassName('heart-li')[0];
 const favoriteButtonLeft = document.getElementsByClassName('favorite-left')[0];
 const shoppingButton = document.getElementsByClassName('shopping-li')[0];
+
+function openOrClose() {
+
+    const menu = document.getElementsByClassName('nav-mobile')[0];
+}
