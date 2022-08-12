0613ddc98901f8b7d69d48d7ce72ce764a626a11
Andreyrickdev <contato-andrey@outlook.com>
2022-08-12 11:53:04 -0300

    selected class shopping-li

--- assets/js/openAndClose.js
+++ assets/js/openAndClose.js
@@ -3,3 +3,4 @@ const searchButton = document.getElementsByClassName('buttonSearch')[0];
 const searchUser = document.getElementsByClassName('user-li')[0];
 const favoriteButton = document.getElementsByClassName('heart-li')[0];
 const favoriteButtonLeft = document.getElementsByClassName('favorite-left')[0];
+const shoppingButton = document.getElementsByClassName('shopping-li')[0];
