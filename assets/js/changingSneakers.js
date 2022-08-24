f9bcb6eead0adea375363967a8cdfbc1d0d12e5c
Andreyrickdev <contato-andrey@outlook.com>
2022-08-24 16:52:26 -0300

    selected black sneaker

--- assets/js/changingSneakers.js
+++ assets/js/changingSneakers.js
@@ -1,5 +1,5 @@
 function changingSneaker () {
 
     const btnOrange = document.querySelector('.buttonOrange');      const sneakerOrange = document.querySelectorAll('.little-sneaker')[0];
-    const btnBlack = document.querySelector('.buttonBlack');        
+    const btnBlack = document.querySelector('.buttonBlack');        const sneakerBlack = document.querySelectorAll('.little-sneaker')[1];
 }
