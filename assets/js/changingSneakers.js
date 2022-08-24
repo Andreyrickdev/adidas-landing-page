efffad1e5007454ee2eaaf57e0942680fbc741b3
Andreyrickdev <contato-andrey@outlook.com>
2022-08-24 16:54:42 -0300

    add event listener click on orange button

--- assets/js/changingSneakers.js
+++ assets/js/changingSneakers.js
@@ -3,4 +3,6 @@ function changingSneaker () {
     const btnOrange = document.querySelector('.buttonOrange');      const sneakerOrange = document.querySelectorAll('.little-sneaker')[0];
     const btnBlack = document.querySelector('.buttonBlack');        const sneakerBlack = document.querySelectorAll('.little-sneaker')[1];
     const btnPurple = document.querySelector('.buttonPurple');      const sneakerPurple = document.querySelectorAll('.little-sneaker')[2];
+
+    btnOrange.addEventListener('click', actionOrange);              
 }
