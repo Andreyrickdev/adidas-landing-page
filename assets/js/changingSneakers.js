2a5e049edb9e5deea6534881c18bf99c0fd044c2
Andreyrickdev <contato-andrey@outlook.com>
2022-08-24 16:51:35 -0300

    selected orange sneaker

--- assets/js/changingSneakers.js
+++ assets/js/changingSneakers.js
@@ -1,4 +1,4 @@
 function changingSneaker () {
 
-    const btnOrange = document.querySelector('.buttonOrange');      
+    const btnOrange = document.querySelector('.buttonOrange');      const sneakerOrange = document.querySelectorAll('.little-sneaker')[0];
 }
