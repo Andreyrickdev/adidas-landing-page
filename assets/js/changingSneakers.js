75f46ce1cd9b037a2bfd0df77a14ed341fe93cff
Andreyrickdev <contato-andrey@outlook.com>
2022-08-24 17:48:20 -0300

    loop structure, removed all class active on img

--- assets/js/changingSneakers.js
+++ assets/js/changingSneakers.js
@@ -156,5 +156,9 @@ function changingSneaker () {
             img.classList.remove('active');
         }
 
+        for (img of imgs7) {
+            img.classList.remove('active');
+        }
+
     }
 }
