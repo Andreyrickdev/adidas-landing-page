520392c2ac6d833541a185ea3664c1878f8ee831
Andreyrickdev <contato-andrey@outlook.com>
2022-08-24 16:52:00 -0300

    selected black button

--- assets/js/changingSneakers.js
+++ assets/js/changingSneakers.js
@@ -1,4 +1,5 @@
 function changingSneaker () {
 
     const btnOrange = document.querySelector('.buttonOrange');      const sneakerOrange = document.querySelectorAll('.little-sneaker')[0];
+    const btnBlack = document.querySelector('.buttonBlack');        
 }
