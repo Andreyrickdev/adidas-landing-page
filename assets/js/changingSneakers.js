ec2995b556d2eafcc0f9753588681cf3b63acc98
Andreyrickdev <contato-andrey@outlook.com>
2022-08-24 16:52:56 -0300

    selected purple button

--- assets/js/changingSneakers.js
+++ assets/js/changingSneakers.js
@@ -2,4 +2,5 @@ function changingSneaker () {
 
     const btnOrange = document.querySelector('.buttonOrange');      const sneakerOrange = document.querySelectorAll('.little-sneaker')[0];
     const btnBlack = document.querySelector('.buttonBlack');        const sneakerBlack = document.querySelectorAll('.little-sneaker')[1];
+    const btnPurple = document.querySelector('.buttonPurple');
 }
