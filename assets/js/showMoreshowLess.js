4c197c4949017a5a952e39543eefdc3f52cf99cd
Andreyrickdev <contato-andrey@outlook.com>
2022-08-25 16:13:11 -0300

    added event listener on button show

--- assets/js/showMoreshowLess.js
+++ assets/js/showMoreshowLess.js
@@ -4,4 +4,9 @@ function showMoreOrLess () {
     const hide = document.getElementsByClassName('hide');
     const photosSneakers = document.getElementsByClassName('photos-sneaker')[0];
     const ulSelected = photosSneakers.getElementsByTagName('ul')[0];
+
+    buttonShow.addEventListener('click', () => {
+
+        
+    });
 }
