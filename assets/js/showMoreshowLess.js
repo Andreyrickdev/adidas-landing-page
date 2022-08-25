fd989df307f30b72d0fff5f1c1951f69fabe5102
Andreyrickdev <contato-andrey@outlook.com>
2022-08-25 16:12:06 -0300

    selected tag

--- assets/js/showMoreshowLess.js
+++ assets/js/showMoreshowLess.js
@@ -3,4 +3,5 @@ function showMoreOrLess () {
     const buttonShow = document.getElementsByClassName('showMoreOrLess')[0];
     const hide = document.getElementsByClassName('hide');
     const photosSneakers = document.getElementsByClassName('photos-sneaker')[0];
+    const ulSelected = photosSneakers.getElementsByTagName('ul')[0];
 }
