d67f5012d3265b48384d4b4075e0fc2693aeea59
Andreyrickdev <contato-andrey@outlook.com>
2022-08-25 15:51:34 -0300

    added class active on imgs

--- assets/js/changingSneakers.js
+++ assets/js/changingSneakers.js
@@ -249,5 +249,6 @@ function changingSneaker () {
         imgs1[2].classList.add('active');
         imgs2[2].classList.add('active');
         imgs3[2].classList.add('active');
+        imgs4[2].classList.add('active');
     }
 }
