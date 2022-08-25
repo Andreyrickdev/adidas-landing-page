e6a4f5ae984252107746307cc1eb04d544d8c2b7
Andreyrickdev <contato-andrey@outlook.com>
2022-08-25 13:52:49 -0300

    loop structure, removed all class active on li

--- assets/js/changingSneakers.js
+++ assets/js/changingSneakers.js
@@ -171,5 +171,10 @@ function changingSneaker () {
         const ulInSneakersModels = document.querySelector('.ul-in-sneakers-models');
 
         const liInUl = ulInSneakersModels.querySelectorAll('li');
+
+        for (li of liInUl) {
+            li.classList.remove('active');
+        }
+
     }
 }
