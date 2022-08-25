6845d47d1ee623f10e930518f8fe517ca8a9a2b0
Andreyrickdev <contato-andrey@outlook.com>
2022-08-25 15:42:16 -0300

    selected all class imgs

--- assets/js/changingSneakers.js
+++ assets/js/changingSneakers.js
@@ -213,5 +213,6 @@ function changingSneaker () {
         const imgs1 = pack1.querySelectorAll('.imgs');
         const imgs2 = pack2.querySelectorAll('.imgs');
         const imgs3 = pack3.querySelectorAll('.imgs');
+        const imgs4 = pack4.querySelectorAll('.imgs');
     }
 }
